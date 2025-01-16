b2743de0c9792e56178f7e5aa800186c760e503b
joselbr2099 <bl455.dj@gmail.com>
2025-01-15 22:05:52 -0400

    add day2 solution

--- day2/testd2.sh
+++ day2/testd2.sh
@@ -1,11 +1,11 @@
  #!/bin/env bash                                                               
                                                                               
 listProcess() {                                                               
-    declare -a list=($1)                                                      
+    declare -a list=($@)                                                      
     declare -i cont=0                                                         
     local comp
     local n=$(echo "${#list[@]}")
-    #echo "${list[@]}"
+    #echo "<> ${list[0]} <<  ${list[$((n-1))]} << $n"
     if (( list[0] > list[$((n-1))] )); then                                    
         comp=true                                                             
     else                                                                      
@@ -39,13 +39,42 @@ listProcess() {
                                                                               
 main() {                         
     local total=0
-    while IFS= read -r line; do                                               
-        safe=$(listProcess "$line")                                                  
-        if [ "$safe" == "safe" ];then
-            ((total++))
-        fi
+    while IFS= read -r cod; do      
+        declare -a list
+        list=()
+
+        for num in $cod;do
+            list+=( $(echo $num) )
+        done
+
+        declare -i n=$(echo "${#list[@]}")
+        declare -a cplist=( "${list[@]}" )
+        declare -i aux=0
+
+        while (( aux <= n ));do
+            echo -e "\ntest: ${cplist[@]}" 
+            #listProcess "$(echo ${cplist[@]})"
+            safe=$(listProcess "$(echo ${cplist[@]})")
+            #listProcess "${cplist[@]}"
+            #echo "rest: $safe" 
+            if [ "$safe" == "safe" ];then
+                ((total++))
+                echo "safe"
+                break
+            else
+                cplist=()
+                for i in $(eval echo {0..$n});do
+                    #echo "item $item - ${list[$aux]} "
+                    if (( $i != $aux ));then
+                        cplist+=(${list[$i]})
+                    fi
+                done
+                ((aux++))
+                #echo " nuevo ${cplist[@]}, aux: $aux"
+            fi
+        done
     done < list
-    echo $total
+    echo "Total safe: $total"
 }                                                                             
                                                                               
 main                                                                           
